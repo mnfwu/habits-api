8dc0bdcd184e5b3b5f47a285dde443f1d7ba2f1d
Matthew Wu <mnfwu12@gmail.com>
2021-07-28 10:15:40 +0800

    add login controller and .env

---
+++ app/controllers/api/v1/login_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::LoginController < Api::V1::BaseController
+  def login
+    p 'logging in'
+    @user = User.find_or_create_by(open_id: wechat_user.fetch('openid'))
+    render json: {
+      user: @user
+    }
+    p "@user: #{@user}"
+  end
+
+  private
+
+  URL = 'https://api.weixin.qq.com/sns/jscode2session'.freeze
+
+  # prepare the params for tencent request
+  def wechat_user
+    wechat_params = {
+      appId: ENV['WECHAT_APP_ID'],
+      secret: ENV['WECHAT_APP_SECRET'],
+      js_code: params[:code],
+      grant_type: 'authorization_code'
+    }
+    # p "wechat_params: #{wechat_params}"
+    wechat_response = RestClient.get(URL, params: wechat_params)
+    # p "wechat_response: #{wechat_response}"
+    JSON.parse(wechat_response.body)
+  end
+end
