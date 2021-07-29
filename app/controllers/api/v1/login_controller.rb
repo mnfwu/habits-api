668b56239d8bc94cea88e790548d11c325a49e4c
Matthew Wu <mnfwu12@gmail.com>
2021-07-29 12:01:13 +0800

    change user validations to enable login on load

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::LoginController < Api::V1::BaseController
   def login
-    p 'logging in'
-    @user = User.find_or_create_by(open_id: wechat_user.fetch('openid'))
+    # p 'logging in'
+    open_id = wechat_user.fetch('openid')
+    puts open_id
+    @user = User.find_or_create_by(open_id: open_id)
+    # p "@user: #{@user.id}"
     render json: {
       user: @user
     }
-    p "@user: #{@user}"
   end
 
   private
@@ -20,9 +22,9 @@ class Api::V1::LoginController < Api::V1::BaseController
       js_code: params[:code],
       grant_type: 'authorization_code'
     }
-    p "wechat_params: #{wechat_params}"
+    # p "wechat_params: #{wechat_params}"
     wechat_response = RestClient.get(URL, params: wechat_params)
-    p "wechat_response: #{wechat_response}"
+    # p "wechat_response: #{wechat_response}"
     JSON.parse(wechat_response.body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
 	has_many :master_habits
 	has_many :users_groups
 	has_many :groups, through: :users_groups
-	validates :display_name, presence: true
+	# validates :display_name, presence: true
 end
