2b53d149308c149cc1162b3fd64cbae8322ee2c3
Dylan Wang <wangvc2017@gmail.com>
2021-07-30 14:08:37 +0800

    create groups works and fixed order

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::GroupsController < Api::V1::BaseController
   def create
     @group = Group.new(group_params)
     if @group.save
+      create_user_group(@group)
       render json: @group
     else
       render_error
@@ -46,5 +47,11 @@ class Api::V1::GroupsController < Api::V1::BaseController
   def render_error
     render json: { errors: @story.errors.full_messages },
       status: :unprocessable_entity
+  end
+
+  def create_user_group(g)
+    @user_id = params[:user_id].to_i
+    @user_group = UsersGroup.new(group_id: g.id, user_id: @user_id)
+    @user_group.save!
   end
 end
