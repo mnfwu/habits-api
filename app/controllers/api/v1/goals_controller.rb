49315a878fc7b43d524938ed127522ae1f0ef823
Dylan Wang <wangvc2017@gmail.com>
2021-08-03 09:58:47 +0800

    added update and destroy goals

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GoalsController < Api::V1::BaseController
+  before_action :find_goal, only: %i[update destroy]
+
 	def create
 		@goal = Goal.new(goal_params)
     @goal.group = Group.find(params[:group_id])
@@ -9,7 +11,23 @@ class Api::V1::GoalsController < Api::V1::BaseController
     end
 	end
 
+  def update
+    if @goal.update(goal_params)
+      render json: @goal
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @goal.destroy
+  end
+
 	private
+
+  def find_goal
+    @goal = Goal.find(params[:id])
+  end
 
 	def goal_params
     params.require(:goal).permit(:end_date, :percent_complete)
