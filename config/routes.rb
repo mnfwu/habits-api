170db64539bd8ae30a8926c38074bf21b6309357
Dylan Wang <wangvc2017@gmail.com>
2021-08-03 10:35:56 +0800

    added user info update

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get 'users/:user_id/groups', to: 'groups#show_user_groups', as: 'show_user_groups'
       get '/users/:user_id/master_habits', to: 'master_habits#show_user_master_habits', as: 'show_user_master_habits'
       post '/login', to: 'login#login'
+      put '/login/users/:user_id', to: 'login#update_user'
       put '/steps/:id', to: 'steps#update'
       post '/groups/:group_id/newuser', to: 'groups#add_user_to_group'
       get 'master_habits/:master_habit_id/analytics', to: 'master_habits#analytics'
